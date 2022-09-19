f1ed782906c0b3abb732d80783bc8f1ec891afe9
Renata <renatasna@msn.com>
2022-09-19 19:01:15 -0300

    praticando com operadores relacionais

---
+++ MaisExerciciosDeLogica/arquivoJSoperacoesAritmeticas.js
@@ -0,0 +1,68 @@
+// //adição
+// var x1,
+//   x2 = 10,
+//   x3 = 12;
+// x1 = x2 + x3;
+// console.log(x1);
+
+// var ch1,
+//   ch2 = "0",
+//   ch3 = "A";
+// ch1 = ch2 + ch3;
+// console.log(ch1);
+
+// var f1,
+//   f2 = 2.5,
+//   f3 = 10.5;
+// f1 = f2 + f3;
+// console.log(f1);
+
+// //subtração
+// var x1,
+//   x2 = 10,
+//   x3 = 12;
+// x1 = x2 - x3;
+// console.log(x1);
+
+// var f1,
+//   f2 = 5.25;
+// f1 = -f2;
+// console.log(f1); // vai retornar -5.25
+
+// //divisão
+
+// var x1,
+//   x2 = 9;
+// var f1, f2;
+// x1 = x2 / 2;
+// console.log(x1);
+
+// f1 = x2 / 2;
+// console.log(f1);
+
+// f2 = x2 / 2.0;
+// console.log(f2);
+
+// // na divisão não podemos guardar o valor em um numero intero e sim em um número real.
+
+// //resto % : mostra o valor que sobre/resto de uma divisão
+// var x1,
+//   x2 = 9;
+// x1 = x2 % 2;
+// console.log(x1);
+
+// x1 = x2 % 5;
+// console.log(x1);
+
+// //PRECEDÊNCIA DOS OPERADORES ARITMÉTICOS: COMO O COMPUTADOR VAI INTERPRETAR A EXPRESSÃO MATEMÁTICA:
+
+// var f1,
+//   f2 = 5.25;
+// f1 = f2 + 10 / 2.0;
+// console.log(f1);
+
+// //precedência : A DIVISÃO É FEITA ANTES DA SOMA SEM O PARENTESES, a soma (adição) smp dentro do parenteses será feita primeiro.
+// var f1,
+//   f2 = 5.25;
+// f1 = (f2 + 10) / 2.0;
+// console.log(f1);
---
+++ MaisExerciciosDeLogica/operadoresRelacionais.js
@@ -0,0 +1,49 @@
+//Operadores relacionais
+var x = 5,
+  y = 3;
+document.write("Resultado : ", x > 4);
+document.write("Resultado : ", x >= y + 2);
+
+var x = 5,
+  y = 3;
+console.log(x > 4); //true
+
+var x = 5,
+  y = 3;
+console.log(x >= y + 2);
+
+var x = 5,
+  y = 3;
+console.log(x > 7);,
+var x = 5,
+  y = 3;
+console.log( x == y + 2);
+
+var x = 5, x = 8;
+console.log(x == 4);
+
+var x = 5, x = 8;
+console.log(x - 2);
+
+var x = 5, x = 8;
+console.log(x = 4);
+
+var x = 12, y = 8
+console.log( x == 5);
+
+var x = 12, y = 8
+console.log( x - 4);
+
+var x = 12, y = 8
+console.log(x + 2 != y);
+
+var x = 12, y = 8
+console.log( x >= 8);
+
+
+var x = 12, y = 8
+console.log (x <= 13);
+
+
+
+
