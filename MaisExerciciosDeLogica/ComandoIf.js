d9a971083f6e7722d71393e31eaf89e90e76597e
Renata <renatasna@msn.com>
2022-09-23 21:22:40 -0300

    comando if

---
+++ MaisExerciciosDeLogica/ComandoIf.js
@@ -0,0 +1,26 @@
+var x = prompt("digite um número inteiro: ", x);
+if (x > 0) {
+  alert("o valor é positivo"); // será executado se a expressão for verdadeira
+}
+alert("fim do programa"); // será executado se a primeira expressão for falta
+
+var x = prompt("digite um numero inteiro", x);
+if (x < 0) {
+  alert("o numero digitado é negativo");
+}
+alert("fim do programa");
+
+var x = prompt("digite um valor inteiro");
+if (x % 2 == 0 || x < 0) {
+  alert("o valor é par ou negativo");
+}
+alert("fim do programa");
+
+var x, y = -1;
+prompt("digite um valor inteiro: ");
+if(x > 0) {
+  y = x % 3;
+  alert("o resto de 3 é: " x,y);
+}
+
+alert("fim do programa")
