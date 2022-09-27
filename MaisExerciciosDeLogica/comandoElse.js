04617182bfc64afc84995f5d252b1c291a987428
Renata <renatasna@msn.com>
2022-09-26 23:07:28 -0300

    media com if else

--- MaisExerciciosDeLogica/comandoElse.js
+++ MaisExerciciosDeLogica/comandoElse.js
@@ -1,26 +1,38 @@
-var x = prompt("Digite um número ", x);
-if (x % 2 == 0) {
-  alert("o valor é par");
+// var x = prompt("Digite um número ", x);
+// if (x % 2 == 0) {
+//   alert("o valor é par");
+// } else {
+//   alert("o valor é ímpar");
+// }
+// alert("Fim do programa");
+
+// var idade = prompt("Digite sua idade", idade);
+// if (idade >= 18) {
+//   alert("Oba, você pode tirar a CNH");
+// } else {
+//   alert("poxa, você é menor de idade!");
+// }
+// alert("Fim do programa");
+
+// var idade = prompt("Digite sua idade", idade);
+// if (idade >= 18) {
+//   alert("Oba, você pode tirar a CNH");
+// } else (x%2 == 1){ var media, n1, n2, n3;
+
+var media, n1, n2, n3;
+
+media = (n1 + n2 + n3) / 3;
+
+media = parseInt(prompt("Digite a nota do primeiro bimestre"));
+
+media = parseInt(prompt("Digite a nota do segundo bimestre"));
+
+media = parseInt(prompt("Digite a nota do terceiro bimestre"));
+
+if (media >= 6) {
+  console.log("Parabéns, você foi aprovado");
 } else {
-  alert("o valor é ímpar");
+  console.log("Poxa, você não foi aprovado");
+
+  console.log("fim do programa");
 }
-alert("Fim do programa");
-
-var idade = prompt("Digite sua idade", idade);
-if (idade >= 18) {
-  alert("Oba, você pode tirar a CNH");
-} else {
-  alert("poxa, você é menor de idade!");
-}
-alert("Fim do programa");
-
-var idade = prompt("Digite sua idade", idade);
-if (idade >= 18) {
-  alert("Oba, você pode tirar a CNH");
-} else (x%2 == 1){ // errado, não colocar um comando dentro do else, pq ele já é o oposto do if.
-  alert("poxa, você é menor de idade!");
-}
-alert("Fim do programa");
-
-
-s
