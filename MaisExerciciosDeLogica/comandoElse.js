98b17a9b587421e9e593483a6d954e627c11eded
Renata <renatasna@msn.com>
2022-09-27 21:56:41 -0300

    praticando com if else

--- MaisExerciciosDeLogica/comandoElse.js
+++ MaisExerciciosDeLogica/comandoElse.js
@@ -1,23 +1,23 @@
-// var x = prompt("Digite um número ", x);
-// if (x % 2 == 0) {
-//   alert("o valor é par");
-// } else {
-//   alert("o valor é ímpar");
-// }
-// alert("Fim do programa");
+var x = prompt("Digite um número ", x);
+if (x % 2 == 0) {
+  alert("o valor é par");
+} else {
+  alert("o valor é ímpar");
+}
+alert("Fim do programa");
 
-// var idade = prompt("Digite sua idade", idade);
-// if (idade >= 18) {
-//   alert("Oba, você pode tirar a CNH");
-// } else {
-//   alert("poxa, você é menor de idade!");
-// }
-// alert("Fim do programa");
+var idade = prompt("Digite sua idade", idade);
+if (idade >= 18) {
+  alert("Oba, você pode tirar a CNH");
+} else {
+  alert("poxa, você é menor de idade!");
+}
+alert("Fim do programa");
 
-// var idade = prompt("Digite sua idade", idade);
-// if (idade >= 18) {
-//   alert("Oba, você pode tirar a CNH");
-// } else (x%2 == 1){ var media, n1, n2, n3;
+var idade = prompt("Digite sua idade", idade);
+if (idade >= 18) {
+  alert("Oba, você pode tirar a CNH");
+} else (x%2 == 1){ var media, n1, n2, n3;
 
 var media, n1, n2, n3;
 
@@ -35,4 +35,31 @@ if (media >= 6) {
   console.log("Poxa, você não foi aprovado");
 
   console.log("fim do programa");
+}
+}
+
+var numeros, n1, n2;
+
+numeros = parseInt(prompt("Digite um numero"));
+numeros = parseInt(prompt("Digite um numero"));
+
+if(n1 > n2) {
+  alert("o maior número digitado é" ,n1)
+}
+
+var num = prompt("Digite um numero");
+if ( num %2 == 0){
+  alert("o numero é par")
+}else 
+  alert("o numero é impar")
+
+
+var n1 = prompt("digite um numero");
+var n2 = prompt("digite um numero");
+if (n1 > n2){
+  alert("o numero maior é" + n1, n2);
+
+}else {
+  if (n1 == n2)
+  alert("os numeros são iguais")
 }
