f7bd3c9ea77b90c752d2624303a8e3879a93a4b2
Renata <renatasna@msn.com>
2022-10-10 22:02:23 -0300

    ignorando a atribuição e pegando a partir da condição

--- MaisExerciciosDeLogica/operadorTernario.js
+++ MaisExerciciosDeLogica/operadorTernario.js
@@ -16,3 +16,10 @@ var z = prompt("digite um valor para z");
 z = x > y ? x : y;
 
 alert("o maior é: " + numeros);
+
+//ignorando a atribuição e pegando a partir da condição
+
+var x = prompt("digite um valor para x");
+var y = prompt("digite um valor para y");
+
+x > y ? alert("x é o maior: ") : alert("y é o maior: ");
