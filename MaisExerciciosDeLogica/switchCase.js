a39efcc1dbd17682f455c3f77e4398a10ed2d0c5
Renata <renatasna@msn.com>
2022-10-10 22:41:04 -0300

    switch case

---
+++ MaisExerciciosDeLogica/switchCase.js
@@ -0,0 +1,15 @@
+var char = prompt("digite um símbolo");
+switch (char) {
+  case ".":
+    alert("Ponto");
+    break;
+  case ",":
+    alert("vírgula");
+    break;
+  case ";":
+    alert("ponto e vírgula");
+    break;
+  case ":":
+    alert("dois pontos");
+    break;
+}
