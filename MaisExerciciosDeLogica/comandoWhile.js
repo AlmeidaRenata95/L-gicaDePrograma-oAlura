59866f22581174b96da1716d1fd06e33d5481235
Renata <renatasna@msn.com>
2022-10-24 23:05:11 -0300

    praticando um pouco mais de while

--- MaisExerciciosDeLogica/comandoWhile.js
+++ MaisExerciciosDeLogica/comandoWhile.js
@@ -5,3 +5,33 @@ while (x < z) {
   x++;
 }
 alert("x agora vale: " + x);
+
+var a = prompt("digite um numero");
+var b = prompt("digite um numero");
+
+while (a < b) {
+  a++;
+}
+alert("a", a);
+
+//comando while repetindo uma condição até determinado valor digitado
+var a = prompt("Digite um valor: ");
+var b = prompt("Digite outro valor");
+
+while (a < b && a > 0) {
+  a++;
+  alert("o valor será: " + a);
+}
+
+var numeros = prompt("digite um numero de 0 a 10");
+while (numeros < 10) {
+  numeros++;
+}
+
+var a = prompt("digite um valor para a:");
+var b = prompt("digite um valor para b:");
+
+while (a < b || a > 0) {
+  a++;
+  alert("o valor será: " + a);
+}
