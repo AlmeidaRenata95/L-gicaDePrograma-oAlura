65d7751515bb6f208d2a84e9accbcd1f04d803e5
Renata <renatasna@msn.com>
2022-11-28 21:29:17 -0300

    aprendendo comando break: O comando break encerra o loop atual

---
+++ MaisExerciciosDeLogica/comandoBreak.js
@@ -0,0 +1,9 @@
+var a = prompt("digite um valor para a : ");
+var b = prompt("digite um valor para b:");
+
+while (a <= b) {
+  a = a + 1;
+  if (a == 5)
+    // se em algum momento meu valor for igual a 5 o break será executado
+    break;
+}
