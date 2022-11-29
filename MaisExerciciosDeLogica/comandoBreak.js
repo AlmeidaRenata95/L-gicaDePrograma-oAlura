cd9525e23067ab88df0c7ff7500d5112d0db24e3
Renata <renatasna@msn.com>
2022-11-28 21:50:33 -0300

    comando break e comando continue

--- MaisExerciciosDeLogica/comandoBreak.js
+++ MaisExerciciosDeLogica/comandoBreak.js
@@ -7,3 +7,16 @@ while (a <= b) {
     // se em algum momento meu valor for igual a 5 o break será executado
     break;
 }
+
+
+
+
+//COMANDO CONTINUE 
+var a = prompt("digite um valor para a : ");
+var b = prompt("digite um valor para b:");
+while ( a ,= b){
+  a =  a + 1;
+  if( a == 5 )
+  continue;
+  alert(" o valor de a é: " + a);
+}
